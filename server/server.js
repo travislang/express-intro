9fb714385e79dcadd334599ec243549fbeb20b96
Travis Lang <tlang50@icloud.com>
2018-11-13 11:56:56 -0600

    add format changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
+
+// require express medules
 let express = require( 'express' );
 
+// run express and save the return class to app
 let app = express( );
 const PORT = 5000;
 
+// use for static assets
 app.use( express.static( 'server/public' ) );
 
+// listening for localhost:5000/vega
+app.get( '/vega', function( req, res ){
+    //say hello to vega
+    let vegaStudents = [ 'said', 'carson', 'john' ];
+    res.send( vegaStudents )
+} );
+
+// listen for traffic on port
 app.listen( PORT, function( ){
     console.log( `listening on port ${PORT}` );
-    
 })
